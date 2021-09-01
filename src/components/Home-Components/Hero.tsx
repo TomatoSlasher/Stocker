e330aabfabbd413597c89dd603ff9fe21545616e
TomatoSlasher <oahwidy7@gmail.com>
2021-09-01 19:34:54 +0300

    full header css

---
+++ src/components/Home-Components/Hero.tsx
@@ -0,0 +1,4 @@
+const Hero = () => {
+  return;
+};
+export default Hero;
---
+++ src/pages/about.tsx
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1>About</h1>
+      <p className="about-text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio velit
+        autem suscipit. Voluptas est consectetur dolorum assumenda accusantium
+        magni, harum eum tempore tenetur labore nihil, in possimus rem sapiente
+        quos, fuga similique voluptates unde optio a ipsam recusandae deserunt?
+        Voluptatum voluptate, nobis accusamus voluptatem modi ipsum praesentium
+        possimus fuga earum eligendi consectetur incidunt molestias saepe sed
+        tempore amet itaque sequi consequatur similique, soluta facilis deleniti
+        adipisci? Optio, aliquid numquam dolorum voluptate impedit minima
+        veritatis recusandae nam, itaque cupiditate rerum ipsum cum amet
+        necessitatibus ad molestias officia. Dolorum possimus expedita nisi quam
+        labore explicabo suscipit consectetur numquam deleniti, porro ipsum
+        ipsam?
+      </p>
+    </div>
+  );
+};
+export default About;
