1dcf00c45a0651057b9e1a42d18d97c5bd9be605
TomatoSlasher <oahwidy7@gmail.com>
2021-09-02 18:16:10 +0300

    new quick search design

--- src/components/Home-Components/StockSearch.tsx
+++ src/components/Home-Components/StockSearch.tsx
@@ -43,15 +43,18 @@ const StockSearch = () => {
     <div className={classes["ticker-search"]}>
       <form className={classes["search-form"]} onChange={tickerChangeHandler}>
         <div className={classes["search-input-container"]}>
-          <button className={classes["search-btn"]} typeof="submit">
-            Search
-          </button>
           <input
-            className={classes["search-input"]}
+            onChange={tickerChangeHandler}
+            placeholder="Quick Search Company"
             type="text"
-            placeholder="search company"
+            className={classes["header-search-input"]}
             ref={tickerChangeRef}
           />
+          <button className={classes["search-button"]}>
+            <svg width="24" height="24" viewBox="0 0 24 24" data-icon="search">
+              <path d="M9 3C5.686 3 3 5.686 3 9c0 3.313 2.686 6 6 6s6-2.687 6-6c0-3.314-2.686-6-6-6m13.713 19.713c-.387.388-1.016.388-1.404 0l-7.404-7.404C12.55 16.364 10.85 17 9 17c-4.418 0-8-3.582-8-8 0-4.42 3.582-8 8-8s8 3.58 8 8c0 1.85-.634 3.55-1.69 4.905l7.403 7.404c.39.386.39 1.015 0 1.403"></path>
+            </svg>
+          </button>
         </div>
       </form>
       {searchQuery ? (
