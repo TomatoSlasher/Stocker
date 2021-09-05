49d178f77ee96ee5f944a97f95c45b3416fa655d
TomatoSlasher <oahwidy7@gmail.com>
2021-09-05 22:22:36 +0300

    overview description more & less buttons

--- src/components/Quote-Sections/Quote-info/OverviewDescription.tsx
+++ src/components/Quote-Sections/Quote-info/OverviewDescription.tsx
@@ -1,11 +1,45 @@
+import { useEffect, useState } from "react";
 import classes from "./OverviewDescription.module.css";
 const OverviewDescription: React.FC<{ description: string }> = (props) => {
+  const [showFull, setshowFull] = useState(false);
+  const [longDesc, setLongDesc] = useState(false);
+  console.log(props.description.length);
+  useEffect(() => {
+    if (props.description.length > 500) {
+      setLongDesc(true);
+    }
+  }, []);
+
   return (
     <div className="description-container">
       <h2>Description</h2>
-      <p className={classes["overview-description"]}>
-        {props.description.substring(0, 500)}
-      </p>
+      {longDesc ? (
+        <div>
+          {showFull ? (
+            <p className={classes["overview-description"]}>
+              {props.description + " "}
+              <span
+                onClick={() => setshowFull(false)}
+                className={classes["more-desc"]}
+              >
+                - less
+              </span>
+            </p>
+          ) : (
+            <p className={classes["overview-description"]}>
+              {props.description.substring(0, 500) + "... "}
+              <span
+                onClick={() => setshowFull(true)}
+                className={classes["more-desc"]}
+              >
+                + more
+              </span>
+            </p>
+          )}
+        </div>
+      ) : (
+        <p className={classes["overview-description"]}>{props.description}</p>
+      )}
     </div>
   );
 };
