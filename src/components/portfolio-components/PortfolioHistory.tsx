1644f8863a4d510586cc8dfcf15a5d5fd92ad5c4
TomatoSlasher <oahwidy7@gmail.com>
2021-09-15 20:49:57 +0300

    responsive trading history

--- src/components/portfolio-components/PortfolioHistory.tsx
+++ src/components/portfolio-components/PortfolioHistory.tsx
@@ -25,11 +25,31 @@ const PorfolioHistory = () => {
               <div className={classes["portfolio-item-header"]}>
                 <span className={classes["logo"]}></span>
                 <div className={classes["portfolio-item-text"]}>
-                  <p className={classes["symbol-date"]}>Symbol/Date</p>
-                  <p className={classes["his-amount"]}>Quantity</p>
-                  <p className={classes["his-amount"]}>Order Type</p>
-                  <p className={classes["his-amount2"]}>Price</p>
-                  <p className={classes["his-amount"]}>Total Cost</p>
+                  <p
+                    className={`${classes.symbol} ${classes["head-text"]} ${classes["symbol-date"]}`}
+                  >
+                    Symbol/Date
+                  </p>
+                  <p
+                    className={`${classes.qty} ${classes["head-text"]} ${classes["his-amount"]}`}
+                  >
+                    Quantity
+                  </p>
+                  <p
+                    className={`${classes.type} ${classes["head-text"]} ${classes["his-amount"]}`}
+                  >
+                    Order Type
+                  </p>
+                  <p
+                    className={`${classes["head-text"]} ${classes["his-amount2"]}`}
+                  >
+                    Price
+                  </p>
+                  <p
+                    className={`${classes.cost} ${classes["head-text"]} ${classes["his-amount"]}`}
+                  >
+                    Total Cost
+                  </p>
                 </div>
               </div>
               <ul className={classes["portfolio-items-list"]}>
@@ -43,13 +63,17 @@ const PorfolioHistory = () => {
                       />
                       <div className={classes["portfolio-item-text"]}>
                         <div className={classes["symbol-date"]}>
-                          <p>{val.symbol}</p>
+                          <h4>{val.symbol}</h4>
                           <p className={classes["portfolio-item-text-date"]}>
                             {val.date}
                           </p>
                         </div>
 
-                        <p className={classes["his-amount"]}>{val.amount}</p>
+                        <p
+                          className={`${classes.amount} ${classes["his-amount2"]}`}
+                        >
+                          {val.amount}
+                        </p>
 
                         <p
                           className={
@@ -61,10 +85,10 @@ const PorfolioHistory = () => {
                           {val.orderType}
                         </p>
                         <p className={classes["his-amount2"]}>
-                          {val.stockPrice}
+                          ${val.stockPrice.toFixed(1)}
                         </p>
                         <p className={classes["his-amount"]}>
-                          {val.orderTotal}
+                          ${val.orderTotal.toFixed(1)}
                         </p>
                       </div>
                     </li>
