2334489272b8a702ff64c5fcf0144891f52e8b25
TomatoSlasher <oahwidy7@gmail.com>
2021-09-18 17:22:21 +0300

    responsive indexes constituents

--- src/components/Stock-List/IndexList.tsx
+++ src/components/Stock-List/IndexList.tsx
@@ -14,15 +14,21 @@ const IndexList: React.FC<{ data: any; title: string }> = (props: {
     <div className={classes["list-wrapper"]}>
       <h1 className={classes["most-title"]}>{props.title}</h1>
       <div className={classes["list-container"]}>
-        <div className={classes["stock-contianer-header"]}>
+        <div
+          className={`${classes["stock-contianer-header"]} ${classes["index-header"]}`}
+        >
           <div className={classes["name-container"]}>
-            <h4 className={classes["symbol"]}>Symbol</h4>
-            <h4 className={classes["comp"]}>Company Name</h4>
+            <h4 className={`${classes["symbol"]} ${classes["symb"]}`}>
+              Symbol
+            </h4>
+            <h4 className={`${classes["comp"]} `}>Company Name</h4>
           </div>
 
           <div className={classes["prices-container"]}>
             <h4 className={classes["sector"]}>Sector</h4>
-            <h4 className={classes["change"]}>Founded</h4>
+            <h4 className={`${classes["ipo"]} ${classes["change"]}`}>
+              IPO Date
+            </h4>
           </div>
         </div>
         <ul className={classes["stock-list-ul"]}>
@@ -35,16 +41,28 @@ const IndexList: React.FC<{ data: any; title: string }> = (props: {
             }) => {
               return (
                 <Link href={`/symbol/${data.symbol.toUpperCase()}/overview`}>
-                  <div className={classes["stock-contianer"]}>
+                  <div
+                    className={`${classes["stock-contianer"]} ${classes["stock-index-contianer"]}`}
+                  >
                     <div className={classes["name-container"]}>
-                      <h4 className={classes["symbol"]}>{data.symbol}</h4>
-                      <p className={classes["symbol2"]}>{data.name}</p>
+                      <h4
+                        className={`${classes["sym"]} ${classes["symb"]}  ${classes["symbol"]}`}
+                      >
+                        {data.symbol}
+                      </h4>
+                      <p className={`${classes["symbol2"]} ${classes.company}`}>
+                        {data.name}
+                      </p>
                     </div>
 
                     <div className={classes["prices-container"]}>
-                      <p className={classes["sector"]}>{data.sector}</p>
-                      <div className={classes["change2"]}>
-                        <p>{data.founded}</p>
+                      <p className={`${classes["sec"]} ${classes["sector"]}`}>
+                        {data.sector}
+                      </p>
+                      <div
+                        className={`${classes["ipo"]} ${classes["change2"]}`}
+                      >
+                        <p className={classes["ipo-date"]}>{data.founded}</p>
                       </div>
                     </div>
                   </div>
